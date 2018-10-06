9475dfab37f1e080013377d96a606ddb71bdd33a
wangjiayu <15270673102@163.com>
2018-10-06 21:54:18 +0800

    提交 usercontoller1

--- src/main/java/com/wangjiayu/springboot/controller/UserControlelr.java
+++ src/main/java/com/wangjiayu/springboot/controller/UserControlelr.java
@@ -1,9 +1,14 @@
 package com.wangjiayu.springboot.controller;
 
+import com.wangjiayu.springboot.service.IUserservice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserControlelr {
+
+    @Autowired
+    private IUserservice userservice;
 
 
 }
