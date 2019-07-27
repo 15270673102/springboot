be4b02ed16b3993de154597d24b0ddb33fd0413a
迅捷羽翼 <15270673102@163.com>
2019-07-27 22:28:47 +0800

    udpate

--- src/main/java/com/wangjiayu/springboot/controller/IndexController.java
+++ src/main/java/com/wangjiayu/springboot/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.wangjiayu.springboot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@Slf4j
 public class IndexController {
 
     @GetMapping(value = "/")
     public String test1() {
+        log.info("xxxxxxxxxxxxxxxxxxxxxxxxx");
+        log.warn("warn warn warn warn warn warn warn warn ");
+        log.error("errorerrorerrorerrorerrorerrorerrorerrorerror ");
         return "hello word";
     }
 
-
-
+    @GetMapping(value = "/2")
+    public String test2() {
+        log.info("ttttttttttttttttttttt");
+        return "hello word";
+    }
 }
