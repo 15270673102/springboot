e8b1aced936bf0b01d8bce20cfb8809ed9178dcd
wangjiayu <15270673102@163.com>
2019-08-10 16:58:15 +0800

    update

--- src/main/java/com/wangjiayu/springboot/util/httprequests.java
+++ src/main/java/com/wangjiayu/springboot/util/httprequests.java
@@ -1,9 +1,10 @@
 package com.wangjiayu.springboot.util;
+
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
-import	java.util.Deque;
-import	java.util.Map;
+import java.util.Deque;
+import java.util.Map;
 
 import net.dongliu.requests.*;
 
@@ -16,14 +17,8 @@ public class httprequests {
     public static void main(String[] args) {
         //demo1();
         //demo2();
-        demo3();
     }
 
-    private static void demo3() {
-        String string = "http://innerserver.thinkoonjt.com/ip-pool/getIPByIndex";
-        String send = Requests.get(string).send().readToText();
-        System.out.println(send);
-    }
 
     private static void demo2() {
         String url = "http://www.baidu.com/s";
